d5f258cca056819aa720e14848917c68e63781fb
NTUC-LFZ <lang.fengzhao@ntuclearninghub.com>
2025-06-05 17:41:09 +0800

    Add travel demo

---
+++ travel/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './App.module.css';
+
+const App = () => {
+  return (
+    <div className={styles.app}>
+      <h1>Rsbuild with React</h1>
+      <p>Start building amazing things with Rsbuild.</p>
+    </div>
+  );
+};
+
+export default App;
