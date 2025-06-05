23d43a6cc70cc0e3780f52e4222ddd1eb5b1597e
NTUC-LFZ <lang.fengzhao@ntuclearninghub.com>
2025-06-05 17:00:36 +0800

    Demo1

--- my-app/src/custom.d.ts
+++ my-app/src/custom.d.ts
@@ -2,3 +2,8 @@ declare module "*.css" {
     const css: { [key: string]: string };
     export default css;
 }
+
+declare module "*.svg" {
+    const css: { [key: string]: string };
+    export default svg;
+}
--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { AppStateProvider } from './AppState';
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
@@ -17,4 +18,8 @@ if (!rootElement) {
 }
 
 const root = ReactDOM.createRoot(rootElement);
-root.render(<App />);
+root.render(
+  <AppStateProvider>
+    <App />
+  </AppStateProvider>
+);
